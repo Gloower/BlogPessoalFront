e1a5c80d3198658bca0df85b78bba014da2e6b95
Gloower <gloowerzin@gmail.com>
2022-09-16 15:54:28 -0300

    testando

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -117,7 +117,7 @@ function Login() {
                 Não tem uma conta?
               </Typography>
             </Box>
-            <Link to="/cadastrousuario" className="text-decorator-none">
+            <Link to="/usuarios/cadastrar" className="text-decorator-none">
               <Typography
                 variant="subtitle1"
                 gutterBottom
